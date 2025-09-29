084e06932d9566e8bc3f6e0a6ffb3ef26d510aa2
Tom Aylott <oblivious@subtlegradient.com>
2025-09-29 12:58:04 -0400

    fix: Update jest.config.js to include transformIgnorePatterns for better module handling

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   preset: 'react-native',
+  transformIgnorePatterns: [
+    'node_modules/(?!(?:\\.pnpm/[^/]+/node_modules/)?(react-native|@react-native|@react-native-community|@react-navigation|@rnx-kit|@react-native-windows|react-native-vector-icons)/)',
+  ],
 };
